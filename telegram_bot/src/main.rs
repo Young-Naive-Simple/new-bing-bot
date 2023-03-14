4e09c204c687bd4649781b556c094da9cd23dd8c
Leo Lu <luyuxuanleo@gmail.com>
2023-03-14 19:30:54 +0800

    fmt fix

--- telegram_bot/src/main.rs
+++ telegram_bot/src/main.rs
@@ -38,7 +38,8 @@ fn msg_mentioned(msg: &Message, username: &str) -> bool {
 }
 
 fn msg_reply_to_id(msg: &Message) -> Option<UserId> {
-    msg.reply_to_message().and_then(|replied| replied.from().map(|replied_from| replied_from.id))
+    msg.reply_to_message()
+        .and_then(|replied| replied.from().map(|replied_from| replied_from.id))
 }
 
 #[tokio::main]
